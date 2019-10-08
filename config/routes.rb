39609e75b3d37fb18c6de55112361db25b6cf497
Scott <Scotthall1983@hotmail.com>
2019-10-08 22:01:53 +0100

    Minor change to the route directory

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'pages#home'
-  get 'about' => 'pages#about'
+  root 'pages_controller#home'
+  get '/about' => 'pages_controller#about'
 end
